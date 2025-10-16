515f893d55ef05697dac0890d15f67736b4ed299
NucaSoft <minesca7@gmail.com>
2025-10-16 19:43:14 -0400

    Ejercicio 1 - Practica 2

---
+++ Unidad 1 - Práctica 2/Ejercicio 1/Controllers/InscripcionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_1.Controllers
+{
+    public class InscripcionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Usuario(string nombre, DateTime fechaNacimiento)
+        {
+            int edad = DateTime.Now.Year - fechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < fechaNacimiento.DayOfYear) edad--;
+
+            ViewBag.Mensaje = $"Hola, {nombre}. Tienes {edad} años y tu año de nacimiento es {fechaNacimiento.Year}.";
+            return View();
+        }
+    }
+}
