7010789c0be6fddc9fa4e1957c0a5ea3622991a9
NucaSoft <minesca7@gmail.com>
2025-10-17 21:13:39 -0400

    Ejercicio 2 - Práctica 2

---
+++ Unidad 1 - Práctica 2/Ejercicio 2/Controllers/OperacionesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_2.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calcular(double a, double b, string operacion)
+        {
+            double resultado = 0;
+
+            switch (operacion)
+            {
+                case "Sumar": resultado = a + b; break;
+                case "Restar": resultado = a - b; break;
+                case "Multiplicar": resultado = a * b; break;
+                case "Dividir": resultado = b != 0 ? a / b : double.NaN; break;
+                case "Potencia": resultado = Math.Pow(a, b); break;
+            }
+
+            ViewBag.Resultado = $"El resultado de {operacion} es: {resultado}";
+            return View();
+        }
+    }
+}
