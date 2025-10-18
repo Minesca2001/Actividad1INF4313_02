7d9e20c43a405277e39069cc5bd585b60c8efc0f
NucaSoft <minesca7@gmail.com>
2025-10-17 21:14:41 -0400

    Ejercicio 4 - Práctica 2

---
+++ Unidad 1 - Práctica 2/Ejercicio 4/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace Ejercicio_4.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Validar(string usuario, string contrasena)
+        {
+            if (usuario == "docente" && contrasena == "P@ssw0rd")
+                return View();
+            else
+                ViewBag.Mensaje = "Usuario incorrecto";
+
+            return View("Index");
+        }
+    }
+}
