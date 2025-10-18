813d4ee1292ba19fa9b58b3b388ee40ce744e63a
NucaSoft <minesca7@gmail.com>
2025-10-17 21:15:09 -0400

    Ejercicio 5 - Práctica 2

---
+++ Unidad 1 - Práctica 2/Ejercicio 5/Controllers/FacturaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Ejercicio_5.Controllers
+{
+    public class FacturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calcular(decimal monto, string opcion)
+        {
+            decimal total = 0, valorAplicado = 0;
+            string tipo = "";
+
+            if (opcion == "ITBIS")
+            {
+                valorAplicado = monto * 0.18m;
+                total = monto + valorAplicado;
+                tipo = "ITBIS (18%)";
+            }
+            else if (opcion == "Descuento")
+            {
+                valorAplicado = monto * 0.15m;
+                total = monto - valorAplicado;
+                tipo = "Descuento (15%)";
+            }
+            var moneda = new CultureInfo("es-DO");
+
+            ViewBag.Mensaje = $"Subtotal: {monto.ToString("C", moneda)}\n" +
+                $"{tipo}: {valorAplicado.ToString("C", moneda)}\n" +
+                $"Total: {total.ToString("C", moneda)}";
+            
+            return View();
+        }
+    }
+}
