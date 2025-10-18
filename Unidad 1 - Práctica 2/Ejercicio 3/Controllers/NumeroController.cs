0691174b0d2ff11fd7d660403e0f16cdc9498145
NucaSoft <minesca7@gmail.com>
2025-10-17 21:14:08 -0400

    Ejercicio 3 - Práctica 2

---
+++ Unidad 1 - Práctica 2/Ejercicio 3/Controllers/NumeroController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_3.Controllers
+{
+    public class NumeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Verificar(int numero)
+        {
+            bool esFibonacci = EsFibonacci(numero);
+            ViewBag.Mensaje = esFibonacci ? $"El {numero} pertenece a la serie de Fibonacci." :
+                                            $"El {numero} no pertenece a la serie de Fibonacci.";
+            return View();
+        }
+
+        private bool EsFibonacci(int n)
+        {
+            int a = 0, b = 1;
+            while (b < n)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b == n || n == 0;
+        }
+    }
+}
